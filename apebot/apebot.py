1533680633ee8f6e4ec655bc28b5cae3c9fdff13
Drew Leonard <andy.j.leonard@gmail.com>
2017-05-11 17:04:35 +0100

    Lots of extra things the bot responds to

--- apebot/apebot.py
+++ apebot/apebot.py
@@ -1,5 +1,8 @@
 from intenter import IntentCaller
 from protocol import MatrixProtocol, IRCProtocol
+import sys
+
+password = sys.argv[1]
 
 
 class Poll():
@@ -47,3 +50,71 @@ class ApeBot:
         Transmit a message into the current room.
         """
         self.protocol.send_message(message)
+
+    def send_emote(self, message):
+        """
+        Transmit a /me style message into the current room.
+        """
+        self.protocol.send_emote(message)
+
+
+
+thisbot = ApeBot(MatrixProtocol("@pyastrobot:matrix.org", password, '#pyastro:matrix.org', "https://matrix.org"))
+
+
+def hide(thisbot, message, sender):
+    thisbot.send_emote('hides')
+
+
+def pingall(thisbot, message, sender):
+    room = thisbot.protocol.room
+    users = room.get_joined_members()
+    for (user, info) in users.items():
+        if 'pyastrobot' not in user:
+            thisbot.send_message('ping {}'.format(user))
+
+
+def newpoll(thisbot, message, sender):
+    room = thisbot.protocol.room
+    question = message.replace('Poll: ', '')
+    thisbot.poll = Poll(thisbot, question)
+    #pingall(thisbot, message, sender)
+    thisbot.send_message(question)
+
+
+def votefor(thisbot, message, sender):
+    room = thisbot.protocol.room
+    response = message.replace('Vote:', '')
+    if not thisbot.poll.fixed:
+        if response not in thisbot.poll.votes.keys():
+            thisbot.poll.votes[response] = 1
+        else:
+            thisbot.poll.votes[response] += 1
+    else:
+        if response not in thisbot.poll.votes.keys():
+            thisbot.send_message("I'm sorry {}, I can't let you do that.".format(sender))
+        else:
+            thisbot.poll.votes[response] += 1
+    thisbot.send_message('Thanks for voting for "{}", {}'.format(response, sender))
+
+
+def showpoll(thisbot, message, sender):
+    thisbot.send_message("Here's the question: {}".format(thisbot.poll.text))
+    thisbot.send_message('Votes so far:\n')
+    thisbot.poll.display()
+
+
+def fixoptions(thisbot, message, sender):
+    thisbot.poll.fixed = True
+    options = message.replace('Options:', '').split(',')
+    for o in options: o.strip()
+    thisbot.poll.votes = {opt: 0 for opt in options}
+
+thisbot.intent.register_keyword('Hide')(hide)
+thisbot.intent.register_keyword('Ping')(pingall)
+thisbot.intent.register_keyword('Poll')(newpoll)
+thisbot.intent.register_keyword('Vote')(votefor)
+thisbot.intent.register_keyword('Show')(showpoll)
+thisbot.intent.register_keyword('Options')(fixoptions)
+
+thisbot.listen()
